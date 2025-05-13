bcccee446338790fdab228b811b79f2ac876992b
Remooze <remusnewell@gmail.com>
2025-05-13 17:57:28 +0800

    Lamp post trigger fix for fps drop prevention

--- Para_Po_Project-v2/Assets/_Scripts/Utilities,Misc,Others/Lamppost.cs
+++ Para_Po_Project-v2/Assets/_Scripts/Utilities,Misc,Others/Lamppost.cs
@@ -2,17 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
 public class Lamppost : MonoBehaviour
 {
     [SerializeField] private GameObject myLight;
+    private bool isNight = false;
+    private bool isPlayerNearby = false;
 
+    private void Awake()
+    {
+        BoxCollider trigger = GetComponent<BoxCollider>();
+        trigger.isTrigger = true;
+
+    }
+
+    private void Start()
+    {
+        myLight.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        isPlayerNearby = true;
+        UpdateLightState();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        isPlayerNearby = false;
+        UpdateLightState();
+    }
+
+    
     public void turnOnLight()
     {
-        myLight.SetActive(true);
+        isNight = true;
+        UpdateLightState();
     }
 
     public void turnOffLight()
     {
-        myLight.SetActive(false);
+        isNight = false;
+        UpdateLightState();
+    }
+
+    private void UpdateLightState()
+    {
+        myLight.SetActive(isNight && isPlayerNearby);
     }
 }
