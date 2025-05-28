e722334c878d40a4413d7d40bd566eabb47552d8
MallorcaAG <yeetsondevyt@proton.me>
2025-05-28 19:47:36 +0800

    Fixed Cutscene issue

--- Para_Po_Project-v2/Assets/_Scripts/ManagerScripts/MainMenuSimulationManager.cs
+++ Para_Po_Project-v2/Assets/_Scripts/ManagerScripts/MainMenuSimulationManager.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class MainMenuSimulationManager : MonoBehaviour
 {
+    [SerializeField] private NPCCount npcs;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        npcs.ResetCurrentNPCValues();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
