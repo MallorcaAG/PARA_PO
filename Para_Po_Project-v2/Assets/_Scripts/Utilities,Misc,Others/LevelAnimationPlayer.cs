232535407c96abd61ea9a9060c88ae5f54bccd3e
Remooze <remusnewell@gmail.com>
2025-05-30 03:20:11 +0800

    added animations per currentiterator
    cant check properly. bug with save data deleted

---
+++ Para_Po_Project-v2/Assets/_Scripts/Utilities,Misc,Others/LevelAnimationPlayer.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class LevelAnimationPlayer : MonoBehaviour
+{
+    [System.Serializable]
+    public class LevelAnimation
+    {
+        public string levelName;          
+        public Animation animationComponent;  
+        public string clipName;                
+    }
+
+    [Tooltip("Assign one animation per level (Level 1 to Level 20)")]
+    [SerializeField] private LevelAnimation[] levelAnimations = new LevelAnimation[20];
+
+    void Start()
+    {
+        int currentLevelIndex = DataManager.Instance.CurrentIterator;
+
+        if (currentLevelIndex < 0 || currentLevelIndex >= levelAnimations.Length)
+        {
+            Debug.LogWarning($"CurrentIterator {currentLevelIndex} is out of bounds");
+            return;
+        }
+
+        PlayLevelAnimation(levelAnimations[currentLevelIndex]);
+    }
+
+    private void PlayLevelAnimation(LevelAnimation levelAnim)
+    {
+        if (levelAnim.animationComponent != null && !string.IsNullOrEmpty(levelAnim.clipName))
+        {
+            levelAnim.animationComponent.Play(levelAnim.clipName);
+            Debug.Log($"Playing clip {levelAnim.clipName} on {levelAnim.animationComponent.gameObject.name} for {levelAnim.levelName}");
+        }
+        else
+        {
+            Debug.LogWarning($"Animation component or clipName missing for {levelAnim.levelName}");
+        }
+    }
+}
