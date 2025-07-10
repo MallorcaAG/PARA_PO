619ad53546b2232d667f134dfa8ddc9281e576fc
MallorcaAG <yeetsondevyt@proton.me>
2025-07-10 14:30:44 +0800

    Prefab'd Traffic Signs

--- Para_Po_Project-v2/Assets/_Scripts/GameMechanicsScripts/TrafficLightViolation.cs
+++ Para_Po_Project-v2/Assets/_Scripts/GameMechanicsScripts/TrafficLightViolation.cs
@@ -4,11 +4,27 @@ public class TrafficLightViolation : MonoBehaviour
 {
     [SerializeField] private GameEvent onRedLightViolation;
 
+    private bool beating = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            beating = true;
+        }
+    }
+
     private void OnTriggerExit(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player") && beating)
         {
             onRedLightViolation.Raise(this, 0);
         }
     }
+
+    private void OnDisable()
+    {
+        beating = false;
+    }
+
 }
